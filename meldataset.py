5ecc184e4e633c5e9fe7c7803622bb5627e48325
unknown <chrishewitt35@hotmail.com>
2024-10-13 20:12:13 +0100

    fix loading in windows

--- meldataset.py
+++ meldataset.py
@@ -138,7 +138,10 @@ class FilePathDataset(torch.utils.data.Dataset):
     def _load_tensor(self, data):
         wave_path, text, speaker_id = data
         speaker_id = int(speaker_id)
-        wave, sr = sf.read(osp.join(self.root_path, wave_path))
+        # .replace() all backslashes with forwardslashes for windows compatibility
+        # wave, sr = sf.read(osp.join(self.root_path, wave_path))
+        file_to_load = osp.join(self.root_path, wave_path).replace("\\","/")
+        wave, sr = sf.read(file_to_load)
         if wave.shape[-1] == 2:
             wave = wave[:, 0].squeeze()
         if sr != 24000:
