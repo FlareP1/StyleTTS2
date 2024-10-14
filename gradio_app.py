cb1d395373189f58c2d49f12ada94c3fcf5689dc
unknown <chrishewitt35@hotmail.com>
2024-10-14 22:41:08 +0100

    Tidy Code and setup better defaults

--- gradio_app.py
+++ gradio_app.py
@@ -1,20 +1,4 @@
-import os
-import re
 import gradio as gr
-#import tempfile
-#from einops import rearrange
-#from vocos import Vocos
-#from pydub import AudioSegment
-#from model import CFM, UNetT, DiT, MMDiT
-#from cached_path import cached_path
-#from model.utils import (
-#    load_checkpoint,
-#    get_tokenizer,
-#    convert_char_to_pinyin,
-#    save_spectrogram,
-#)
-#from transformers import pipeline
-#import librosa
 import click
 
 import torch
@@ -28,10 +12,8 @@ random.seed(0)
 import numpy as np
 np.random.seed(0)
 
-import time
 import random
 import yaml
-from munch import Munch
 import numpy as np
 #import torch
 from torch import nn
@@ -92,57 +74,6 @@ print(f"Using {device} device")
 import phonemizer
 global_phonemizer = phonemizer.backend.EspeakBackend(language='en-us', preserve_punctuation=True,  with_stress=True)
 
-""" gr.Info("Loading Models...")
-config = yaml.safe_load(open("Models/LibriTTS/config.yml"))
-
-# load pretrained ASR model
-ASR_config = config.get('ASR_config', False)
-ASR_path = config.get('ASR_path', False)
-text_aligner = load_ASR_models(ASR_path, ASR_config)
-
-# load pretrained F0 model
-F0_path = config.get('F0_path', False)
-pitch_extractor = load_F0_models(F0_path)
-
-# load BERT model
-from Utils.PLBERT.util import load_plbert
-BERT_path = config.get('PLBERT_dir', False)
-plbert = load_plbert(BERT_path)
-
-model_params = recursive_munch(config['model_params'])
-model = build_model(model_params, text_aligner, pitch_extractor, plbert)
-_ = [model[key].eval() for key in model]
-_ = [model[key].to(device) for key in model]
-
-params_whole = torch.load("Models/LibriTTS/epochs_2nd_00020.pth", map_location='cpu',weights_only=True)
-params = params_whole['net']
-
-for key in model:
-    if key in params:
-        print('%s loaded' % key)
-        try:
-            model[key].load_state_dict(params[key])
-        except:
-            from collections import OrderedDict
-            state_dict = params[key]
-            new_state_dict = OrderedDict()
-            for k, v in state_dict.items():
-                name = k[7:] # remove `module.`
-                new_state_dict[name] = v
-            # load params
-            model[key].load_state_dict(new_state_dict, strict=False)
-#             except:
-#                 _load(params[key], model[key])
-_ = [model[key].eval() for key in model]
-
-from Modules.diffusion.sampler import DiffusionSampler, ADPM2Sampler, KarrasSchedule
-sampler = DiffusionSampler(
-    model.diffusion.diffusion,
-    sampler=ADPM2Sampler(),
-    sigma_schedule=KarrasSchedule(sigma_min=0.0001, sigma_max=3.0, rho=9.0), # empirical parameters
-    clamp=False
-) """
-
 gr.Info("Loading Punkt...")
 import nltk
 nltk.download('punkt_tab')
@@ -154,9 +85,6 @@ sampler=None
 gModelType=None
 
 def normalize_audio(samples):
-    # Convert the samples to a numpy array for easier manipulation
-    #samples = np.array(samples, dtype=np.float32)
-
     # Normalize the audio by dividing by the maximum absolute value
     max_amplitude = np.max(np.abs(samples))
     if max_amplitude > 0:
@@ -501,166 +429,6 @@ def inferenceWithOutRef(text, noise, diffusion_steps=5, embedding_scale=1, speed
         
     return out.squeeze().cpu().numpy()
 
-#pipe = pipeline(
-#    "automatic-speech-recognition",
-#    model="openai/whisper-large-v3-turbo",
-#    torch_dtype=torch.float16,
-#    device=device,
-#)
-
-# --------------------- Settings -------------------- #
-
-#target_sample_rate = 24000
-#n_mel_channels = 100
-#hop_length = 256
-#target_rms = 0.1
-#nfe_step = 32  # 16, 32
-#cfg_strength = 2.0
-#ode_method = "euler"
-#sway_sampling_coef = -1.0
-#speed = 1.0
-# fix_duration = 27  # None or float (duration in seconds)
-#fix_duration = None
-
-
-""" def load_model(exp_name, model_cls, model_cfg, ckpt_step):
-    ckpt_path = str(cached_path(f"hf://SWivid/F5-TTS/{exp_name}/model_{ckpt_step}.safetensors"))
-    #ckpt_path = f"ckpts/{exp_name}/model_{ckpt_step}.safetensors"  # .pt | .safetensors
-    vocab_char_map, vocab_size = get_tokenizer("Emilia_ZH_EN", "pinyin")
-    model = CFM(
-        transformer=model_cls(
-            **model_cfg, text_num_embeds=vocab_size, mel_dim=n_mel_channels
-        ),
-        mel_spec_kwargs=dict(
-            target_sample_rate=target_sample_rate,
-            n_mel_channels=n_mel_channels,
-            hop_length=hop_length,
-        ),
-        odeint_kwargs=dict(
-            method=ode_method,
-        ),
-        vocab_char_map=vocab_char_map,
-    ).to(device)
-
-    model = load_checkpoint(model, ckpt_path, device, use_ema = True)
-
-    return model """
-
-
-# load models
-#F5TTS_model_cfg = dict(
-#    dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4
-#)
-#E2TTS_model_cfg = dict(dim=1024, depth=24, heads=16, ff_mult=4)
-
-#F5TTS_ema_model = load_model(
-#    "F5TTS_Base", DiT, F5TTS_model_cfg, 1200000
-#)
-#E2TTS_ema_model = load_model(
-#    "E2TTS_Base", UNetT, E2TTS_model_cfg, 1200000
-#)
-
-
-""" def infer(ref_audio_orig, ref_text, gen_text, exp_name, remove_silence, speed):
-    print(gen_text)
-    if len(gen_text) > 40000:
-        raise gr.Error("Please keep your text under 40000 chars.")
-    gr.Info("Converting audio...")
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
-        aseg = AudioSegment.from_file(ref_audio_orig)
-        audio_duration = len(aseg)
-        if audio_duration > 15000:
-            gr.Warning("Audio is over 15s, clipping to only first 15s.")
-            aseg = aseg[:15000]
-        aseg.export(f.name, format="wav")
-        ref_audio = f.name
-    if exp_name == "F5-TTS":
-        ema_model = F5TTS_ema_model
-    elif exp_name == "E2-TTS":
-        ema_model = E2TTS_ema_model
-
-    if not ref_text.strip():
-        gr.Info("No reference text provided, transcribing reference audio...")
-        ref_text = outputs = pipe(
-            ref_audio,
-            chunk_length_s=30,
-            batch_size=128,
-            generate_kwargs={"task": "transcribe"},
-            return_timestamps=False,
-        )["text"].strip()
-        gr.Info("Finished transcription")
-    else:
-        gr.Info("Using custom reference text...")
-    audio, sr = torchaudio.load(ref_audio)
-    if audio.shape[0] > 1:
-        audio = torch.mean(audio, dim=0, keepdim=True)
-
-    rms = torch.sqrt(torch.mean(torch.square(audio)))
-    if rms < target_rms:
-        audio = audio * target_rms / rms
-    if sr != target_sample_rate:
-        resampler = torchaudio.transforms.Resample(sr, target_sample_rate)
-        audio = resampler(audio)
-    audio = audio.to(device)
-
-    # Prepare the text
-    text_list = [ref_text + gen_text]
-    final_text_list = convert_char_to_pinyin(text_list)
-
-    # Calculate duration
-    ref_audio_len = audio.shape[-1] // hop_length
-    # if fix_duration is not None:
-    #     duration = int(fix_duration * target_sample_rate / hop_length)
-    # else:
-    zh_pause_punc = r"。，、；：？！"
-    ref_text_len = len(ref_text) + len(re.findall(zh_pause_punc, ref_text))
-    gen_text_len = len(gen_text) + len(re.findall(zh_pause_punc, gen_text))
-    #duration = ref_audio_len + int(ref_audio_len / ref_text_len * gen_text_len / speed)
-    duration = int(ref_text_len / ref_audio_len  * gen_text_len / speed  )
-    duration = int( duration * target_sample_rate / hop_length )
-    gr.Info(f"Estimated Audio Length {duration}")
-
-    # inference
-    gr.Info(f"Generating audio using {exp_name}")
-    with torch.inference_mode():
-        generated, _ = ema_model.sample(
-            cond=audio,
-            text=final_text_list,
-            duration=duration,
-            steps=nfe_step,
-            cfg_strength=cfg_strength,
-            sway_sampling_coef=sway_sampling_coef,
-        )
-
-    generated = generated[:, ref_audio_len:, :]
-    generated_mel_spec = rearrange(generated, "1 n d -> 1 d n")
-    gr.Info("Running vocoder")
-    vocos = Vocos.from_pretrained("charactr/vocos-mel-24khz")
-    generated_wave = vocos.decode(generated_mel_spec.cpu())
-    if rms < target_rms:
-        generated_wave = generated_wave * rms / target_rms
-
-    # wav -> numpy
-    generated_wave = generated_wave.squeeze().cpu().numpy()
-
-    if remove_silence:
-        gr.Info("Removing audio silences... This may take a moment")
-        non_silent_intervals = librosa.effects.split(generated_wave, top_db=30)
-        non_silent_wave = np.array([])
-        for interval in non_silent_intervals:
-            start, end = interval
-            non_silent_wave = np.concatenate(
-                [non_silent_wave, generated_wave[start:end]]
-            )
-        generated_wave = non_silent_wave
-
-    # spectogram
-    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp_spectrogram:
-        spectrogram_path = tmp_spectrogram.name
-        save_spectrogram(generated_mel_spec[0].cpu().numpy(), spectrogram_path)
-
-    return (target_sample_rate, generated_wave), spectrogram_path
- """
 
 with gr.Blocks() as app:
     gr.Markdown(
@@ -689,11 +457,11 @@ If you're having issues, try converting your reference audio to WAV or MP3, clip
     load_btn = gr.Button("Load Model", variant="primary")
     generate_btn = gr.Button("Synthesize", variant="primary")
     with gr.Accordion("Advanced Settings", open=False):
-        alpha = gr.Number(value=0.3,label="Alpha",step=0.1)
-        beta = gr.Number(value=0.7,label="Beta",step=0.1)
+        alpha = gr.Number(value=0.1,label="Alpha: 0 = closer to speaker (timbre) reference ",step=0.1)
+        beta = gr.Number(value=0.3,label="Beta : 0 = closer to speaker emotion (prosody) reference ",step=0.1)
         diffusion_steps=gr.Number(value=10,label="Steps")
-        embedding_scale=gr.Number(value=1.5,label="Embedding Scale")
-        speed=gr.Number(value=1,label="Speed Up",step=0.05)
+        embedding_scale=gr.Number(value=3.5,label="Embedding Scale : Emotion")
+        speed=gr.Number(value=1.3,label="Speed Up",step=0.05)
         
         normalise_output = gr.Checkbox(
             label="Normalise Output",
@@ -754,7 +522,6 @@ def main(port, host, share, api):
     app.queue(api_open=api).launch(
         server_name=host, server_port=port, share=share, show_api=api
     )
-
 
 if __name__ == "__main__":
     main()
